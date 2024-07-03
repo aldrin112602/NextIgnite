d49b345246c51815d52106659329dccfb36fa449
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-07-03 16:39:57 +0800

    URIHelper modified: throwing Exeption added

--- app/Helpers/UriHelper.php
+++ app/Helpers/UriHelper.php
@@ -15,39 +15,31 @@ class UriHelper
  */
 public static function getParams($uri)
 {
-    // Check if the provided URI is valid
     if (!filter_var($uri, FILTER_VALIDATE_URL)) {
         throw new \Exception('Invalid URI');
     }
 
-    // Extract the query string from the URI
     $params = parse_url($uri, PHP_URL_QUERY);
-
-    // Parse the query string into an associative array
     parse_str($params, $paramsArray);
-
-    // Return the associative array of parameters
     return $paramsArray;
 }
-
 
     /**
  * This function removes parameters from a given URI.
  *
  * @param string $uri The URI from which to remove parameters.
  * @return string The URI without any parameters.
- *
  * @throws \Exception If the provided URI is not valid.
+ *
  */
 public static function removeParams($uri)
 {
-    // Parse the provided URI into its components
+    if (!filter_var($uri, FILTER_VALIDATE_URL)) {
+        throw new \Exception('Invalid URI');
+    }
+    
     $parsedUrl = parse_url($uri);
-
-    // Extract the path component from the parsed URL
     $url = $parsedUrl['path'];
-
-    // Return the URI without any parameters
     return $url;
 }
 }
