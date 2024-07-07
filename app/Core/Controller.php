371ebfb43ef90f1a68dcb23d27daf6e62c86cae4
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-07-07 19:37:51 +0800

    Fixed routing

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -7,6 +7,13 @@ class Controller
     protected function view($view, $data = [])
     {
         extract($data);
-        require "../app/Views/{$view}.php";
+        $viewPath = __DIR__ . "/../../app/Views/{$view}.nxt.php";
+
+        // Check if the view file exists and require it
+        if (file_exists($viewPath)) {
+            require_once $viewPath;
+        } else {
+            throw new \Exception("View file not found: {$viewPath}");
+        }
     }
 }
