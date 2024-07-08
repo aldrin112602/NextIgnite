04cbe44ef9b5882e0d2d762d7e8147438e961850
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-07-08 14:38:05 +0800

    Modified Request

--- app/Http/Request.php
+++ app/Http/Request.php
@@ -2,9 +2,36 @@
 
 namespace App\Http;
 
-class Request 
+class Request
 {
-    public function validation($requiredData = []): array|string|\Error {
-        return [];
+    protected $method;
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+    }
+
+    public function validation($requiredData = []): array
+    {
+        $sanitized = [];
+        switch ($this->method) {
+            case 'GET':
+                $sanitized = [];
+                foreach ($requiredData as $key => $value) {
+                    if (!isset($_GET[$key])) throw new \Error("Undefined key `$key`");
+                }
+                break;
+            case  'POST':
+                $sanitized  = [];
+                foreach ($requiredData as $key => $value) {
+                    $splitvalue = explode('|', $value);
+
+                    if (in_array('required', $splitvalue) && empty($_POST[$key])) throw new \Error("`$key` is required");
+                    $sanitized[$key] = trim($_POST[$key]);
+                }
+                break;
+        }
+
+
+        return $sanitized;
     }
 }
