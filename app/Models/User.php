e54faf2daf83517bb076f7bfdbf2d650e7f333cd
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-07-09 09:38:55 +0800

    Fix Model

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class User extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'username',
+        'email',
+        'password',
+        'role'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+}
--- app/Models/UserModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Models;
-
-class UserModel
-{
-    // Define your model properties and methods here
-}
