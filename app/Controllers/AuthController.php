89d96cc7547901c5e9748d664318dbc50cb50f6c
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-07-09 08:13:24 +0800

    Upp

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -21,11 +21,25 @@ class AuthController extends Controller
     {
         $request = new Request();
         $validated = $request->validation([
-            "email" => "required|unique",
-            "password" => "required|min=6,max=20"
+            "email" => "required",
+            "password" => "required"
         ]);
 
-        var_dump($validated);
     }
 
+    public function handleSignup()
+    {
+        $request = new Request();
+        $validated = $request->validation([
+            "username" => "required|min=6|max=20",
+            "email" => "required",
+            "password" => "required|min=6|max=20"
+        ]);
+
+        // Save to database
+        // $user = new User($validated);
+        // $user->save();
+
+        
+    }
 }
--- app/Views/auth/signup.nxt.php
+++ app/Views/auth/signup.nxt.php
@@ -31,22 +31,22 @@
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                         Username
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="username" id="username" type="text" placeholder="Username">
                 </div>
                 <div class="mb-4">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                         Email
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" id="email" type="email" placeholder="Email">
                 </div>
                 <div class="mb-6">
                     <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                         Password
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="******************">
                 </div>
                 <div class="flex items-center justify-between">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                         Sign Up
                     </button>
                 </div>
